f3a354894a943df45c4d497ebd4fb60592c80559
Isaac <shocker945516@gmail.com>
2023-05-15 11:16:36 -0700

    About and Hero

--- src/layouts/About.tsx
+++ src/layouts/About.tsx
@@ -1,7 +1,11 @@
 const About = () => {
   return (
     <section id="about" className="">
-      <div>About</div>
+      <div className="flex flex-col ">
+        <div className="flex flex-col justify-center basis-24">
+          <p className="text-3xl font-semibold">About</p>
+        </div>
+      </div>
     </section>
   );
 };
--- src/layouts/Hero.tsx
+++ src/layouts/Hero.tsx
@@ -2,23 +2,47 @@ import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
-    <motion.section
-      initial="hidden"
-      whileInView="visible"
-      variants={{
-        hidden: { opacity: 0, x: -200 },
-        visible: { opacity: 1, x: 0 },
-      }}
-      transition={{ duration: 1 }}
-      id="main"
-      className="flex flex-row items-center min-h-screen"
-    >
-      <div className="container flex flex-col items-center py-5">
-        <p className="text-4xl font-notoSans">HI! I AM</p>
-        <p className="text-5xl ">Isaac Wu</p>
-        <p className="text-2xl ">A Web Developer</p>
+    <section id="main" className="flex flex-row items-center min-h-screen">
+      <div className="flex flex-col gap-5">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          variants={{
+            hidden: { opacity: 0, x: -400 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          transition={{ duration: 1 }}
+          className=""
+        >
+          <span className="text-9xl font-notoSans">HI!</span>
+          <span className="text-3xl "> I AM </span>
+        </motion.div>
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          variants={{
+            hidden: { opacity: 0, x: 300 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          transition={{ duration: 1 }}
+          className="text-9xl "
+        >
+          Isaac Wu
+        </motion.p>
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          variants={{
+            hidden: { opacity: 0, x: 300 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          transition={{ duration: 1 }}
+          className="text-8xl "
+        >
+          A Web Developer
+        </motion.p>
       </div>
-    </motion.section>
+    </section>
   );
 };
 
