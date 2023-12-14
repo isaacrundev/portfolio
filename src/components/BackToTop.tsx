771bf0f1a309ed00ca79e8b4d6e72a7ae1aa0caf
shocker945516 <shocker945516@gmail.com>
2023-12-14 19:14:52 +0800

    Add Youbike and changed to top icon

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -1,3 +1,4 @@
+import { ArrowUpFromLine } from "lucide-react";
 import { topBtn } from "../assets/index.ts";
 
 const BackToTop = () => {
@@ -9,13 +10,22 @@ const BackToTop = () => {
       type="button"
       data-mdb-ripple="true"
       data-mdb-ripple-color="light"
-      // className="fixed inline-block p-1 uppercase transition duration-150 ease-in-out bg-gray-200 rounded-full shadow-md w-14 h-14 hover:bg-gray-300 hover:shadow-lg focus:bg-gray-200 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-300 active:shadow-lg bottom-5 right-5"
       id="btn-back-to-top"
       onClick={handleClick}
-      className="fixed inline-block p-1 uppercase transition duration-150 ease-in-out bg-white rounded-full shadow-md w-14 h-14 hover:bg-gray-300 hover:shadow-lg focus:bg-gray-200 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-300 active:shadow-2xl bottom-5 right-5"
+      // className="fixed inline-block p-1 uppercase transition duration-150 ease-in-out bg-white rounded-full shadow-md w-14 h-14 hover:shadow-lg focus:bg-gray-200 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-300 active:shadow-2xl bottom-5 right-5"
+      className="fixed flex items-center justify-center text-black transition duration-150 ease-in-out bg-white rounded-full shadow-md hover:font-bold w-14 h-14 bottom-5 right-5"
     >
       {/* <img src={topBtn} className="object-contain bg-transparent w-9" /> */}
-      <img src={topBtn} className="object-contain w-8 bg-white" />
+      {/* <img src={topBtn} className="object-contain w-8 bg-white" /> */}
+      {/* <div className="flex flex-col bg-white"> */}
+      <span className="z-50 bg-inherit">
+        <ArrowUpFromLine
+          className="bg-transparent"
+          stroke="#322f2f"
+          strokeWidth={3}
+        />
+      </span>
+      {/* </div> */}
     </button>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,17 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: { sans: ["Noto Sans", ...defaultTheme.fontFamily.sans] },
     extends: {
+      fontFamily: {
+        // sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        pressStart2p: ["Press Start 2P", ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         black: "#1a1313",
         white: "#f0ebeb",
       },
+      plugins: [],
     },
-    extend: {
-      fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-        notoSans: ["Noto Sans", "sans-serif"],
-      },
-    },
-    plugins: [],
   },
 };
